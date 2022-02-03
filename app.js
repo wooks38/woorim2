530897929ea096a1bbc7a1df1b0009b833678ec1
DWP <wooks38@gmail.com>
2022-02-03 14:33:07 +0900

    initial

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+//view setting
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+
+//루트로 들어오면 해당 경로 이하 js파일을 찾아서 읽음
+const home = require("./routes/home")
+app.use("/", home); //use는 미들웨어를 사용하는 메서드
+
+module.exports = app;
