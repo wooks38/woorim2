0a585e138149e487c7979745a5e0aa091d83f4c9
DWP <wooks38@gmail.com>
2022-02-03 17:24:54 +0900

    20200203_v1”

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require("express");
 const app = express();
 
 //view setting
-app.set("views", "./src/views");
+app.set("views", "./views");
 app.set("view engine", "ejs");
 
 //루트로 들어오면 해당 경로 이하 js파일을 찾아서 읽음
-const home = require("./src/routes/home")
+const home = require("./routes/home")
 app.use("/", home); //use는 미들웨어를 사용하는 메서드
 
 //static 폴더 지정
